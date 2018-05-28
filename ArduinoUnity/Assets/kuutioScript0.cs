18517960a485d075e32caa157ccc493076df0349
VeniBoi <35433655+VeniBoi@users.noreply.github.com>
2018-05-28 15:51:02 +0300

    Projektin siirto githubiin

---
+++ ArduinoUnity/Assets/kuutioScript0.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kuutioScript0 : MonoBehaviour {
+
+
+	public int T;
+	public int arvo;
+	public Color startColor;
+	public Color endColor;
+
+	// Use this for initialization
+	void Start () {
+		//T = GameObject.Find("GameManager").GetComponent<ArduinoScript>().S0;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//GetComponent<Renderer>().material.color = new Color32(T, 152, 0, 255);
+		T = GameObject.Find("GameManager").GetComponent<ArduinoScript>().S0;
+		arvo = T;
+		if (T > 0)
+		{
+			//GetComponent<Renderer>().material.color = new Color32(T, 255 - T, 0, 255);
+		}
+
+		transform.position = new Vector3(-5.9f, -arvo/15, 0.84f);
+	}
+}
